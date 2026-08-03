896098c2cc83a6287f566cda4ab1cd1bf05dba5a
marianaferreiraaa <mcferreiramunoz@gmail.com>
2026-08-02 22:37:51 -0400

    Update productos.js

--- productos.js
+++ productos.js
@@ -50,13 +50,11 @@ const productos = [
     },
 
     {
-        nombre: "Calvin Klein In2U for Him 100 ml",
+        nombre: "Calvin Klein In2U 100 ml for Him",
         notas: "Frescura, Cítricos",
         precio: "$35.000",
         imagen: "imagenes/ck.png"
-    },
-
-
+    }
 
 
 
